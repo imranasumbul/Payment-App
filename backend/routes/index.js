11d18870417382e0239a0126a676bd8550424a62
Imrana Sumbul <imranasumbul1211@gmail>
2024-07-23 12:06:19 +0530

    removed unnecessary things

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,11 +5,7 @@ const accountRouter = require("./accountRouter");
 
 const rootRouter = express.Router();
 
-rootRouter.get('/', function(req, res){
-    res.json({
-        msg: "from /api/v1"
-    })
-})
+
 rootRouter.use('/users', userRouter);
 rootRouter.use('/accounts', accountRouter);
 module.exports = rootRouter;
